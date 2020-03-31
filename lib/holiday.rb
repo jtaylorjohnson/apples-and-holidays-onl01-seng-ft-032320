b74df92331cbafed6778f35457e180166881f46d
James Johnson <jtaylorjohnson@gmail.com>
2020-03-31 02:22:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].each do |holiday, supplies|
-    return supplies
+    
   end
 end
 
