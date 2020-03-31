3623afa363991ef82bb26ee7b4bfdb9c8e8b185c
James Johnson <jtaylorjohnson@gmail.com>
2020-03-31 03:22:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+array []
+holiday_hash.each do |season, holiday|
+  season.each do |holiday, string|
+    if string == "BBQ"
+      array << holiday
+    end
+  end
 end
 
 
